551ab54d46c5450a78627a504f195b34396a9bc0
Arturo Romero Marquez <arturo@customer-187-217-219-195.uninet-ide.com.mx>
2019-08-14 15:00:39 -0500

    new user dialog

--- src/views/UserList/components/UserCreate/UserCreate.js
+++ src/views/UserList/components/UserCreate/UserCreate.js
@@ -1,134 +1,116 @@
-import React, { Component, Fragment, useState } from 'react'
-import { Button, TextField, Grid } from '@material-ui/core'
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import { makeStyles } from '@material-ui/styles';
-
-
-
+import React, { Component, Fragment, useState } from "react";
+import { Button, TextField, Grid } from "@material-ui/core";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import { makeStyles } from "@material-ui/styles";
 
 export default class extends Component {
+  state = { open: false };
 
+  handleToggle = () => {
+    this.setState({
+      open: !this.state.open
+    });
+  };
 
-	state = { open: false }
+  render() {
+    const { open } = this.state;
+    const location = [
+      {
+        value: "merida",
+        label: "Merida"
+      },
+      {
+        value: "new-york",
+        label: "New York"
+      },
+      {
+        value: "san-francisco",
+        label: "San Francisco"
+      }
+    ];
+    return (
+      <Fragment>
+        <button
+          className="MuiButtonBase-root MuiButton-root MuiButton-contained MuiButton-containedPrimary"
+          onClick={this.handleToggle}
+        >
+          Add User +{" "}
+        </button>
 
-	handleToggle = () => {
-		this.setState({
-			open: !this.state.open
-		})
-	}
+        <Dialog
+          open={open}
+          onClose={this.handleToggle}
+          aria-labelledby="form-dialog-title"
+        >
+          <DialogTitle id="form-dialog-title">Add New User</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              To create a new user, please enter the necessary data.
+            </DialogContentText>
+            <Grid item md={12} xs={12}>
+              <TextField
+                autoFocus
+                margin="normal"
+                id="name"
+                label="Name"
+                type="text"
+                fullWidth
+              />
+            </Grid>
+            <Grid item md={12} xs={12}>
+              <TextField
+                margin="normal"
+                id="email"
+                label="Email Address"
+                type="email"
+                fullWidth
+              />
+            </Grid>
 
-
-	render(){
-		const { open } = this.state;
-		const location = [
-	    {
-	      value: 'alabama',
-	      label: 'Alabama'
-	    },
-	    {
-	      value: 'new-york',
-	      label: 'New York'
-	    },
-	    {
-	      value: 'san-francisco',
-	      label: 'San Francisco'
-	    }
-	  ];
-		return <Fragment>
-		<button className="MuiButtonBase-root MuiButton-root MuiButton-contained MuiButton-containedPrimary" onClick={this.handleToggle}>Add User + </button>
-		
-		 <Dialog open={open} onClose={this.handleToggle} aria-labelledby="form-dialog-title">
-	        <DialogTitle id="form-dialog-title">Add New User</DialogTitle>
-	        <DialogContent>
-	          <DialogContentText>
-	            To create a new user, please enter the necessary data.
-	          </DialogContentText>
-	          <Grid
-              item
-              md={12}
-              xs={12}
-            >
-	          <TextField
-	            autoFocus
-	            margin="normal"
-	            id="name"
-	            label="Name"
-	            type="text"
-	            fullWidth
-	            
-	          />
-	          </Grid>
-	          <Grid
-              item
-              md={12}
-              xs={12}
-            >
-	        <TextField	      
-	            margin="normal"
-	            id="email"
-	            label="Email Address"
-	            type="email"
-	            fullWidth
-	            
-	          />
-	          </Grid>
-	 
-	          <Grid
-              item
-              md={12}
-              xs={12}
-            >
-	          <TextField
+            <Grid item md={12} xs={12}>
+              <TextField
                 fullWidth
                 label="Select Location"
                 margin="normal"
-                name="location"             
+                name="location"
                 required
                 select
                 SelectProps={{ native: true }}
                 value={location.value}
                 variant="outlined"
               >
-              <option></option>
+                <option></option>
                 {location.map(option => (
-
-                  <option
-                    key={option.value}
-                    value={option.value}
-                  >
+                  <option key={option.value} value={option.value}>
                     {option.label}
                   </option>
                 ))}
               </TextField>
-              </Grid>  
-	          <Grid
-              item
-              md={12}
-              xs={12}
-            >
-	          <TextField	      
-	            margin="normal"
-	            id="phone"
-	            label="Phone"
-	            type="number"
-	            fullWidth
-	            
-	          />
-	          </Grid>
-	        </DialogContent>
-	        <DialogActions>
-	          <Button onClick={this.handleToggle} color="primary">
-	            Cancel
-	          </Button>
-	          <Button onClick={this.handleToggle} color="primary">
-	            Subscribe
-	          </Button>
-	        </DialogActions>
-	      </Dialog>
-	    </Fragment>
-	}
+            </Grid>
+            <Grid item md={12} xs={12}>
+              <TextField
+                margin="normal"
+                id="phone"
+                label="Phone"
+                type="number"
+                fullWidth
+              />
+            </Grid>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.handleToggle} color="primary">
+              Cancel
+            </Button>
+            <Button onClick={this.handleToggle} color="primary">
+              Save
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </Fragment>
+    );
+  }
 }
